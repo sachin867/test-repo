8db3e3449494ed650a4bfee1c8363ba87d332131
sachin867 <sachindurgad.sd@gmail.com>
2025-06-01 15:43:12 +0530

    modified changes

--- test.java
+++ test.java
@@ -2,5 +2,7 @@ public class test{
 public static void main(String atgs[]){
     System.out.print("Hello world");
      System.out.print("Hello world");
+      System.out.print("Hello world");
+       System.out.print("Hello world");
 }
 }
